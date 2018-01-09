eac1b3e7d9b349524bfeb2f6f81015f276fda11b
ApolloDevelopment <clutch_forum@yahoo.com>
2018-01-08 16:50:11 -0800

    Update 4
    - FINALLY GOT THE MF TRIG ADDED IN THIS BIHHHH YUHHH!!
    - (Added Trig in lmao. Too lazy to add it to index so just look for the syntax in the source)

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-console.log('Functions:\n\n- firstLine() //logs the pre-loaded line to the console that states the available functions\n\n- getHypotenuse(a, b) //gets the value of the hypotenuse with the given A and B leg lengths\n\n- getMissingLeg(b, c) //gets the length of a missing side if the hypotenuse is given\n\n- getRelation(a, b, c) //gets the relationship of a triangle (obtuse, acute, right) based on its length measurements\n');
+console.log('Functions Loaded...\n\nWrite "firstLine();" to log available functions.');
 function firstLine() {
   var firstLine = console.log('Functions:\n\n- getHypotenuse(a, b) //gets the value of the hypotenuse with the given A and B leg lengths\n\n- getMissingLeg(b, c) //gets the length of a missing side if the hypotenuse is given\n\n- getRelation(a, b, c) //gets the relationship of a triangle (obtuse, acute, right) based on its length measurements\n');
   return firstLine;
@@ -69,5 +69,51 @@ function getRsquared(a,b,c) {
   } else {
     relation = "Right";
     return relation;
+  }
+}
+
+
+
+
+/////////////complex math (trig) starts here///////////////
+
+
+function Trig(theta, ang2, opp, adj, hyp) {
+  var ans = undefined;
+  if(opp == 'x' && adj != null) {
+    ans = (Math.tan(theta)) * adj;
+    return "x = " +ans;
+  } else if(adj == 'x' && opp != null) {
+    ans = opp / (Math.tan(theta));
+    return "x = " +ans;
+  } else if(opp == 'x' && hyp != null) {
+    ans = (Math.sin(theta)) * hyp;
+    return "x = " +ans;
+  } else if(hyp == 'x' && opp != null) {
+    ans = opp / (Math.sin(theta));
+    return "x = " +ans;
+  } else if(adj == 'x' && hyp != null) {
+    ans = (Math.cos(theta)) * hyp;
+    return "x = " +ans;
+  } else if(hyp == 'x' && adj != null) {
+    ans = adj / (Math.cos(theta));
+    return "x = " +ans;
+  } else if(theta == 'x' && ang2 != null) {
+    ans =  180 - (ang2 + 90);
+    return "x = " +ans;
+  } else if(ang2 == 'x' && theta != null) {
+    ans = 180 - (theta + 90);
+    return "x = " +ans;
+  } else if(theta == 'x' && hyp != null && opp != null) {         //Multiply the result by 180/Math.PI to convert from radians to degrees.
+    ans = (Math.asin(opp / hyp)) * (180/Math.PI);
+    return "x = " +ans;
+  } else if(theta == 'x' && hyp != null && adj != null) {
+    ans = (Math.acos(adj / hyp)) * (180/Math.PI);
+    return "x = " +ans;
+  } else if(theta == 'x' && opp != null && adj != null) {
+    ans = (Math.atan(opp / adj)) * (180/Math.PI);
+    return "x = " +ans;
+  } else if(theta != 'x' && ang2 != 'x' && opp != 'x' && adj != 'x' && hyp != 'x') {
+    console.log("Please be sure to make one of the inputs 'x' so the function can find the value of that variable.");
   }
 }
